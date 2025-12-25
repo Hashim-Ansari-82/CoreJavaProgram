c058e53d17583b12eeeb352bdc646c026f292f51
Hashim Ansari <mohammadhashim7922@gmail.com>
2025-12-25 20:04:14 +0530

    Delete src/com directory

--- src/com/logicalprogram/SquareByCommandLineArgs.java
+++
@@ -1,8 +0,0 @@
-package com.logicalprogram;
-
-class SquareByCommandLineArgs{
-    public static void main(String[] args){
-		int n=Integer.parseInt(args[0]);
-		System.out.println("The Square of "+n+" is "+(n*n));
-	}
-}
--- src/com/numericpattern/NumericPattern6th.java
+++
@@ -1,7 +0,0 @@
-package com.numericpattern;
-
-public class NumericPattern6th {
-    public static void main(String[] args) {
-
-    }
-}
